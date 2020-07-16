8a63c6fbdb365462e987e561d3f3c25ab7eb6fe1
Paddy <pacurtin@tcd.ie>
2020-07-16 16:05:39 -0500

    Init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div id="heading">
+        Code Challenge - Blue Squad
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
